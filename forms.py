f85a52bc7431eca8de2d73c2402481bcf95f1571
Mary Hubert <mhubert314@gmail.com>
2024-01-29 13:57:41 -0800

    second commit for 2nd assignment

--- forms.py
+++ forms.py
@@ -1,1 +1,11 @@
 # helper file that ensures that users are giving us the right data, like a valid email address
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class UserLoginForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    submit_button = SubmitField()
+
